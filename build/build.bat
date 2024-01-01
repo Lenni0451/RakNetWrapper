f52af38c2ef426b438c17905607f464cd24f6af3
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-01-01 22:40:39 +0100

    Moved build scripts

--- build/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-cl ..\raknet_c\*.cpp /O2 /I ..\fb-raknet\Source\ /D_USRDLL /D_WINDLL /LD ..\fb-raknet\Source\*.cpp /MT /link ws2_32.lib /EXPORT:RN_RakPeerGetInstance /EXPORT:RN_RakPeerDestroyInstance /EXPORT:RN_RakPeerStartup /EXPORT:RN_RakPeerConnect /EXPORT:RN_RakPeerShutdown /EXPORT:RN_RakPeerReceive /EXPORT:RN_RakPeerDeallocatePacket /EXPORT:RN_RakPeerSend /EXPORT:RN_SetRakNetProtocolVersion
-del *.obj
--- build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-g++ -Os -ffunction-sections -fdata-sections -s -shared -fPIC ../fb-raknet/Source/*.cpp ../raknet_c/*.cpp -I ../fb-raknet/Source -o raknet.so -fvisibility=hidden
-mv raknet.so raknet-x86-64.so
-g++ -m32 -Os -ffunction-sections -fdata-sections -s -shared -fPIC ../fb-raknet/Source/*.cpp ../raknet_c/*.cpp -I ../fb-raknet/Source -o raknet.so -fvisibility=hidden
-mv raknet.so raknet-x86.so
