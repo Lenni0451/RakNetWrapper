9cedbc7e95df84a7f3cf56a47849033558d730ed
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-01-06 00:24:03 +0100

    Upload entire target folder

--- build/rust/build.bat
+++ build/rust/build.bat
@@ -2,4 +2,4 @@
 
 cd ..\..\raknet_rs
 cargo build --release
-rename target\release\librust_raknet.dll target\release\librust-raknet.dll
+REM rename target\release\librust_raknet.dll target\release\librust-raknet.dll
