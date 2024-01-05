7b9d1cbe561721ada2ada03f3b121ee375245c13
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-01-06 00:03:55 +0100

    Cross compile to 32 bit

--- build/rust/build.sh
+++ build/rust/build.sh
@@ -2,4 +2,9 @@
 
 cd ../../raknet_rs
 cargo build --release
-mv ../../raknet_rs/target/release/librust_raknet.so ../../raknet_rs/target/release/librust-raknet.so
+mv ../../raknet_rs/target/release/librust_raknet.so ../../raknet_rs/target/release/librust-raknet-x64.so
+
+rustup default stable
+rustup target add i686-unknown-linux-musl
+cargo build --release --target i686-unknown-linux-musl
+mv ../../raknet_rs/target/i686-unknown-linux-musl/release/librust_raknet.so ../../raknet_rs/target/i686-unknown-linux-musl/release/librust-raknet-x64_86.so
